13491a02c7ee6d861ae358c0dcc236b8745d2281
hasan <mdhasanhk.98@gmail.com>
2022-12-26 21:01:33 +0600

    forEach

--- starter/script.js
+++ starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /*
 /////////////////////////////////////////////////
@@ -129,5 +129,31 @@ console.log(arr.at(-1)); //another way
 
 console.log('jonas'.at(0)); //at method also works on string
 console.log('jonas'.at(-1));
-////////////////////////////////////////
+/////////////////////////////////////////
+*/
+
+/*
+/////////////////////////////////////////
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements) {
+for (const [i, movement] of movements.entries()) {
+  if (movement > 0) {
+    console.log(`movement: ${i + 1} You deposited: ${movement}`);
+  } else {
+    console.log(`movement: ${i + 1} You withdrew: ${Math.abs(movement)}`);
+  }
+}
+
+//FOREACH
+console.log('----FOREACH----');
+
+movements.forEach(function (mov, i, arr) {
+  if (mov > 0) {
+    console.log(`movement: ${i + 1} You deposited: ${mov}`);
+  } else {
+    console.log(`movement: ${i + 1} You withdrew: ${Math.abs(mov)}`);
+  }
+});
+///////////////////////////////////////
 */
