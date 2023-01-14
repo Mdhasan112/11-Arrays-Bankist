55b5294dca5df4fb5ac1ace90e075958b8f95e51
hasan <mdhasanhk.98@gmail.com>
2023-01-14 23:54:33 +0600

    done

--- starter/script.js
+++ starter/script.js
@@ -200,7 +200,7 @@ currenciesUnique.forEach(function (value, _value, map) {
 // Coding Challenge #1
 
 Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
-
+ 
 Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
 
 1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
@@ -237,7 +237,7 @@ const checkDogs = function (dogsJulia, dogsKate) {
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
-//////////////////////////////////////////
+/////////////////////////////////////////
 */
 
-//////////////////////////////////////////
+/////////////////////////////////////////
