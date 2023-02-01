863f532bdb5a27effc391eb793374cb804b46b34
hasan <mdhasanhk.98@gmail.com>
2023-02-01 19:01:43 +0600

    challenge- 2 completed

--- starter/script.js
+++ starter/script.js
@@ -343,6 +343,7 @@ console.log(max);
 ////////////////////////////////////////
 */
 
+/*
 ///////////////////////////////////////
 // Coding Challenge #2
 
@@ -358,12 +359,20 @@ Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages (
 
 TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
 TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
-*/
+GOOD LUCK 😀
+
 
 const calcAverageHumanAge = function (ages) {
   const humanAges = ages.map(age => (age <= 2 ? 2 * age : 16 + age * 4));
+  console.log(humanAges);
   const adults = humanAges.filter(age => age >= 18);
-  console.log(humanAges, adults);
+  console.log(adults);
+  const average = adults.reduce((acc, age) => acc + age, 0) / adults.length;
+  return average;
 };
 
-calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg1 = calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]);
+const avg2 = calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]);
+console.log(avg1, avg2);
+///////////////////////////////////////
+*/
