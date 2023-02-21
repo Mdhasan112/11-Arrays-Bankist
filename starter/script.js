9f7f6dbd88bec76a1cbd8c68d948134c403b5e6f
hasan <mdhasanhk.98@gmail.com>
2023-02-22 00:55:33 +0600

    applying flat and flatMap method

--- starter/script.js
+++ starter/script.js
@@ -557,3 +557,33 @@ console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 /////////////////////////////////////////
 */
+
+/*
+////////////////////////////////////////
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2, 3]], [[4, 5, 6]], [7], 8];
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+// const overallBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(overallBalance);
+
+//Flat
+const overallBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance);
+
+//FlatMap                               //FlatMap works only one level deep
+const overallBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overallBalance2);
+/////////////////////////////////////
+*/
